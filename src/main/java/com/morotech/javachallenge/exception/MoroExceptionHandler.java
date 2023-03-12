b2067877aa8069ebb63c86101cc9ee105d8c3eeb
geokall <gkallimanis@iknowhow.com>
2023-03-12 21:01:17 +0200

    minor

--- src/main/java/com/morotech/javachallenge/exception/MoroExceptionHandler.java
+++ src/main/java/com/morotech/javachallenge/exception/MoroExceptionHandler.java
@@ -5,16 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNullApi;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -60,7 +57,7 @@ public class MoroExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleGenericException(Exception e) {
+    public ResponseEntity<MoroResponseError> handleGenericException(Exception e) {
         LOGGER.error(e.getMessage());
 
         String error = "There was an error. Please contact your administrator " + e.getMessage();
