cd6b99d459a418e853219bf8f76276f97f0e7d7d
geokall <gkallimanis@iknowhow.com>
2023-03-15 00:35:31 +0200

    removed unused on cache config and minor renames

--- src/main/java/com/morotech/javachallenge/config/CacheConfig.java
+++ src/main/java/com/morotech/javachallenge/config/CacheConfig.java
@@ -3,49 +3,32 @@ package com.morotech.javachallenge.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.management.timer.Timer;
 
+import java.time.LocalDateTime;
+
 import static com.morotech.javachallenge.utils.MoroConstant.CACHE_BOOKS;
+import static com.morotech.javachallenge.utils.MoroConstant.CACHE_BOOK_DETAILS;
 
 @Configuration
 public class CacheConfig {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CacheConfig.class);
 
-    private static final long ONE_DAY = Timer.ONE_DAY;
-
-    private final CacheManager cacheManager;
+    private static final long TWELVE_HOURS_IN_MILLISECONDS = Timer.ONE_HOUR * 12;
 
     @Autowired
-    public CacheConfig(CacheManager cacheManager) {
-        this.cacheManager = cacheManager;
+    public CacheConfig() {
     }
 
-    @Scheduled(fixedRate = ONE_DAY)
-    @CacheEvict(value = CACHE_BOOKS, allEntries = true)
+    @Scheduled(fixedRate = TWELVE_HOURS_IN_MILLISECONDS)
+    @CacheEvict(cacheNames = {CACHE_BOOKS, CACHE_BOOK_DETAILS}, allEntries = true)
     public void clearSoftOneCacheValues() {
-        LOGGER.info("Cleared softOne cached values");
-    }
-
-//    @Scheduled(fixedRate = ONE_DAY)
-//    public void evictAllCachesPerDay() {
-//        evictAllCaches();
-//    }
-
-    private void evictAllCaches() {
-        cacheManager.getCacheNames().parallelStream()
-                .forEach(cacheName -> {
-                    Cache cache = cacheManager.getCache(cacheName);
-
-                    if (cache != null) {
-                        cache.clear();
-                    }
-                });
+        LOGGER.info("Cleared cached books and book details at: " + LocalDateTime.now());
+        LOGGER.info("Next clear cache will happen at: " + LocalDateTime.now().plusHours(12));
     }
 }
